2d64141024988712361a1861138862618b00a1b5
jgomez <juan.gomez01@gmail.com>
2016-10-27 21:55:15 -0500

    se inserta en la BD los archivos y luego se consulta la URL

--- JS/archivos.js
+++ JS/archivos.js
@@ -6,38 +6,65 @@ $(function(){
 
 	function uploadFile(e){
 
-		console.log("evento");
-
 		//Obtener archivo
 		var file = e.target.files[0];
 
-		// Crear referencia a storage (Directorio creado en firebase para subir los archivos)
-		var storageRef = firebase.storage().ref("ImgsPreguntas/" + file.name);
+		// Crear referencia al storage (Directorio creado en firebase para subir los archivos)
+		var refStorage = firebase.storage().ref("imgsPreguntas/"+file.name);
 
 		// Subir archivo y regresa tarea de estado
-		var task = storageRef.put(file);
+		var task = refStorage.put(file);
+
+		var database = firebase.database();
 
 		// Actualizamos progreso
-		task.on('state_changed', 
+		task.on('state_changed',
 
-			function progress(snapshot){
-				var porcentaje = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-				console.log("Estado > " + snapshot.state);
-				$('#uploader').val(porcentaje);
-			},
+				function progress(snapshot){
+					var porcentaje = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+					console.log(snapshot.state);
+					$('#uploader').val(porcentaje);
+				},
 
-			function error(err){
-			 alert("Error subiendo archivo > " + err.message);
+				function error(err){
+					alert("Error > "+err.message);
+				},
 
-			},
+				function complete(){
+					console.log(task.snapshot.downloadURL);
 
-			function complete(){
-				console.log(task.snapshot.downloadURL);
-			}
+					var ref = database.ref('files');
 
-		);
+					ref.push({
+						nombre:file.name,
+						url:task.snapshot.downloadURL
+					});
+
+				}
+			);
 
 	}
+
+	function addItem(nombreArchivo, url){
+		var plantilla = '<li><a href="'+url+'">'+nombreArchivo+'</a></li>'
+		$("#listaArchivos").append(plantilla);
+	}
+
+	var database = firebase.database();
+	var ref = database.ref("files");
+
+	ref.on("child_added", function(snapshot){
+		var valores = snapshot.val();
+		var refFile = firebase.storage().ref("imgsPreguntas/"+valores.nombre);
+
+		refFile.getDownloadURL().then(function(url){
+			addItem(valores.nombre, url);
+		}).catch(function(error){
+			alert("Error > "+error.message);
+		});
+
+		
+	});
 
 
 
